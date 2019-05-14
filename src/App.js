9e849565dfbfdeede57b01236f8abcf3d3155f41
rockman25 <pschirmer70@gmail.com>
2019-05-14 06:48:28 -0400

    fixed first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nats lose and Cubs win!
         </p>
         <a
           className="App-link"
